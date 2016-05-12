2b9603ca487e6e6a73c047ec539ba226b14a04df
David Daly <12dd01@kzoo.edu>
2016-05-12 13:17:26 -0400

    Add files via upload

--- prime_number_finder.py
+++ prime_number_finder.py
@@ -1,12 +1,18 @@
-# Prime number finder!
-
-list_of_primes = []
-def  main():
+def  list_all_primes_below(limit):
+    list_of_primes = []
     number = 2
-    while number <= 1000000:
+    while number <= limit:
             is_prime(number)
             number += 1
-    print list_of_primes
+    return list_of_primes
+
+def  list_first_X_primes(X):
+    list_of_primes = []
+    number = 2
+    while len(list_of_primes) < X:
+            is_prime(number)
+            number += 1
+    return list_of_primes
 def is_prime(number):
     for prime in list_of_primes:
         if number % prime == 0:
@@ -17,4 +23,6 @@ def is_prime(number):
 def store_prime(prime):
     list_of_primes.append(prime)
 
-main()
+if __name__ == "__main__":
+    #print(list_all_primes_below(10000))
+    print(list_first_X_primes(10001))
